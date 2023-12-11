69a30dbf8328fc595cf0f1522f69e7259509ea6e
hijas <hijasahamed2000@gmail.com>
2023-12-11 11:13:25 +0530

    latest

--- lib/screens/add_donor.dart
+++ lib/screens/add_donor.dart
@@ -1,4 +1,5 @@
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class Addblooduser extends StatefulWidget {
@@ -12,6 +13,26 @@ class _AddblooduserState extends State<Addblooduser> {
 
   final bloodGroups=['A+','A-','B+','B-','O+','O-','AB+','AB-'];
   String ? selectedGroup;
+  final CollectionReference donor= FirebaseFirestore.instance.collection('blooddonation');
+
+  void addDoner(){
+    final data={
+      'name':donerName.text,
+      'age':donerAge.text,
+      'mobile':donerMobile.text,
+      'district':donerDistrict.text,
+      'village':donerVillage.text,
+      'blood':selectedGroup,
+     };
+     donor.add(data);
+     Navigator.pop(context);
+  }
+
+  TextEditingController donerName=TextEditingController();
+  TextEditingController donerAge=TextEditingController();
+  TextEditingController donerMobile=TextEditingController();
+  TextEditingController donerDistrict=TextEditingController();
+  TextEditingController donerVillage=TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -29,6 +50,7 @@ class _AddblooduserState extends State<Addblooduser> {
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: TextFormField(
+                  controller: donerName,
                   decoration: const InputDecoration(
                     border: OutlineInputBorder(),
                     label: Text('Donor Name') 
@@ -39,16 +61,19 @@ class _AddblooduserState extends State<Addblooduser> {
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: TextFormField(
+                  controller: donerAge,
                   decoration: const InputDecoration(
                     border: OutlineInputBorder(),
                     label: Text('Age') 
                   ),
+                  keyboardType: TextInputType.number,
                   textCapitalization: TextCapitalization.words,
                 ),
               ),
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: TextFormField(
+                  controller: donerMobile,
                   decoration: const InputDecoration(
                     border: OutlineInputBorder(),
                     label: Text('Mobile') 
@@ -60,6 +85,7 @@ class _AddblooduserState extends State<Addblooduser> {
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: TextFormField(
+                  controller: donerDistrict,
                   decoration: const InputDecoration(
                     border: OutlineInputBorder(),
                     label: Text('District') 
@@ -70,6 +96,7 @@ class _AddblooduserState extends State<Addblooduser> {
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: TextFormField(
+                  controller: donerVillage,
                   decoration: const InputDecoration(
                     border: OutlineInputBorder(),
                     label: Text('Village') 
@@ -93,7 +120,9 @@ class _AddblooduserState extends State<Addblooduser> {
               ),
               const SizedBox(height: 10,),
               ElevatedButton(
-                onPressed: (){},
+                onPressed: (){
+                  addDoner();
+                },
                 style:const ButtonStyle(
                   minimumSize: MaterialStatePropertyAll(Size(200, 50)),
                   backgroundColor: MaterialStatePropertyAll(Colors.red)
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -28,24 +28,46 @@ class _HomescreenState extends State<Homescreen> {
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       body: StreamBuilder(
-        stream: donor.snapshots(), 
+        stream: donor.orderBy('name').snapshots(), 
         builder: (context,AsyncSnapshot snapshot){
           if(snapshot.hasData){
-            return ListView.separated(
-              itemBuilder: (ctx,index){
-                final DocumentSnapshot donorsnap=snapshot.data.docs[index];
-                return Column(
-                  children: [
-                    Text(donorsnap['name']),
-                    Text(donorsnap['village']),
-                    Text(donorsnap['district']),
-                  ],
-                );
-              }, 
-              separatorBuilder: (ctx,index){
-                return const  SizedBox();
-              }, 
-              itemCount: snapshot.data!.docs.length, 
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ListView.separated(
+                itemBuilder: (ctx,index){
+                  final DocumentSnapshot donorsnap=snapshot.data.docs[index];
+                  return Container(
+                    height: 125,
+                    color: Color.fromARGB(255, 241, 237, 237),
+                    child:  Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        SizedBox(
+                          child: CircleAvatar(
+                            radius: 50,
+                            child: Text(donorsnap['blood'],style: TextStyle(fontSize: 30),),
+                          ),
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('Name:${donorsnap['name']}'),
+                            Text('Age:${donorsnap['age'].toString()}'),
+                            Text('Mobile:${donorsnap['mobile'].toString()}'),
+                            Text('District:${donorsnap['district']}'),
+                            Text('Village:${donorsnap['village']}'),
+                            Text('Village:${donorsnap['blood']}'),
+                          ],
+                        ),
+                      ],
+                    ),
+                  );
+                }, 
+                separatorBuilder: (ctx,index){
+                  return const  SizedBox(height: 10,);
+                }, 
+                itemCount: snapshot.data!.docs.length, 
+              ),
             );
           }
           return const SizedBox(
